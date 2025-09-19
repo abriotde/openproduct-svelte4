161cba6c2dd14de19c0dc40c8f79b879627e7343
alberic <alberic.briot@osiris-team.fr>
2025-09-19 16:05:31 +0200

    Better.

--- src/routes/auth/oauth/google/callback/+page.server.ts
+++ src/routes/auth/oauth/google/callback/+page.server.ts
@@ -29,71 +29,70 @@ export async function load(event) {
 		const storedState = event.cookies.get('google_state') ?? null;
 		const storedCodeVerifier = event.cookies.get('google_code_verifier') ?? null;
 		if (!code || !storedState || !storedCodeVerifier || state !== storedState) {
-		return {
-			status: 400
-		};
-	}
-	try {
-		const tokens = await googleOauth.validateAuthorizationCode(code, storedCodeVerifier);
-		const response = await fetch('https://openidconnect.googleapis.com/v1/userinfo', {
-			headers: {
-				Authorization: `Bearer ${tokens.accessToken}`
-			}
-		});
-		const googleUser = (await response.json()) as GoogleUser;
-		const existingUser = await getUserByEmail(googleUser.email);
-
-		if (existingUser) {
-			const session = await lucia.createSession(existingUser.id, {});
-			const sessionCookie = lucia.createSessionCookie(session.id);
-			event.cookies.set(sessionCookie.name, sessionCookie.value, {
-				path: '.',
-				...sessionCookie.attributes
+			return {status: 400};
+		}
+		try {
+			const tokens = await googleOauth.validateAuthorizationCode(code, storedCodeVerifier);
+			const response = await fetch('https://openidconnect.googleapis.com/v1/userinfo', {
+				headers: {
+					Authorization: `Bearer ${tokens.accessToken}`
+				}
 			});
-		} else {
-			//new User
-			const token = crypto.randomUUID();
-			const id = crypto.randomUUID();
-			const newGoogleUser = {
-				id: id,
-				provider: 'google',
-				providerId: googleUser.sub,
-				email: googleUser.email.toLowerCase(),
-				firstName: googleUser.given_name,
-				lastName: googleUser.family_name,
-				role: 'USER',
-				verified: false,
-				receiveEmail: true,
-				token: token
-			};
-			const newUser = await createUser(newGoogleUser);
-			if (newUser) {
-				await sendVerificationEmail(newUser.email, token);
-				const session = await lucia.createSession(newUser.id, {});
+			const googleUser = (await response.json()) as GoogleUser;
+			const existingUser = await getUserByEmail(googleUser.email);
+
+			if (existingUser) {
+				const session = await lucia.createSession(existingUser.id, {});
 				const sessionCookie = lucia.createSessionCookie(session.id);
 				event.cookies.set(sessionCookie.name, sessionCookie.value, {
 					path: '.',
 					...sessionCookie.attributes
 				});
-				setFlash(
-					{
-						type: 'success',
-						message: 'Account created. Please check your email to verify your account.'
-					},
-					event
-				);
+			} else {
+				//new User
+				const token = crypto.randomUUID();
+				const id = crypto.randomUUID();
+				const newGoogleUser = {
+					id: id,
+					provider: 'google',
+					providerId: googleUser.sub,
+					email: googleUser.email.toLowerCase(),
+					firstName: googleUser.given_name,
+					lastName: googleUser.family_name,
+					role: 'USER',
+					verified: false,
+					receiveEmail: true,
+					token: token
+				};
+				const newUser = await createUser(newGoogleUser);
+				if (newUser) {
+					await sendVerificationEmail(newUser.email, token);
+					const session = await lucia.createSession(newUser.id, {});
+					const sessionCookie = lucia.createSessionCookie(session.id);
+					event.cookies.set(sessionCookie.name, sessionCookie.value, {
+						path: '.',
+						...sessionCookie.attributes
+					});
+					setFlash(
+						{
+							type: 'success',
+							message: 'Account created. Please check your email to verify your account.'
+						},
+						event
+					);
+				}
 			}
-		}
-		return redirect(302, '/dashboard');
-	} catch (e) {
-		if (e instanceof OAuth2RequestError && e.message === 'bad_verification_code') {
-			// invalid code
+			return redirect(302, '/dashboard');
+		} catch (e) {
+			if (e instanceof OAuth2RequestError && e.message === 'bad_verification_code') {
+				// invalid code
+				return {
+					status: 400
+				};
+			}
 			return {
-				status: 400
+				status: 500
 			};
 		}
-		return {
-			status: 500
-		};
 	}
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
 
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 const config = {
 	darkMode: ["class"],
 	content: ["./src/**/*.{html,js,svelte,ts}"],
@@ -15,43 +15,43 @@ const config = {
 		},
 		extend: {
 			colors: {
-				border: "hsl(var(--border) / <alpha-value>)",
-				input: "hsl(var(--input) / <alpha-value>)",
-				ring: "hsl(var(--ring) / <alpha-value>)",
-				background: "hsl(var(--background) / <alpha-value>)",
-				foreground: "hsl(var(--foreground) / <alpha-value>)",
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",				
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
 				primary: {
-					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))"
 				},
 				secondary: {
-					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
-					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))"
 				},
 				destructive: {
-					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))"
 				},
 				muted: {
-					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
-					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))"
 				},
 				accent: {
-					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
-					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))"
 				},
 				popover: {
-					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
-					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))"
 				},
 				card: {
-					DEFAULT: "hsl(var(--card) / <alpha-value>)",
-					foreground: "hsl(var(--card-foreground) / <alpha-value>)"
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))"
 				}
 			},
 			borderRadius: {
 				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
+				md: "calc(var(--radius) - 2px)",				
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
@@ -62,3 +62,4 @@ const config = {
 };
 
 export default config;
+
