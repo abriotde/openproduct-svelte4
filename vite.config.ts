3eb3e8512d7de77a496d25e51cc07e7aa48cb8d2
alberic <alberic.briot@osiris-team.fr>
2025-09-26 09:11:16 +0200

    First step to add SkeletonUI.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+// const tailwindcss = require('tailwindcss');
 
 export default defineConfig({
 	plugins: [
+		// tailwindcss(),
 		enhancedImages(), // must come before the SvelteKit plugin
 		sveltekit()
 	]
