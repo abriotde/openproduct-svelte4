ea95a81d99b95642833ad779d8c48596ab1afad8
alberic <alberic.briot@osiris-team.fr>
2025-10-16 09:46:13 +0200

    Complete migration: Rename first_name and last_name to firstname and lastname.

--- scripts/db_dump.sh
+++ scripts/db_dump.sh
@@ -4,4 +4,4 @@ source ../.env.local
 
 PGPASSWORD="$DATABASE_PASSWORD"
 export PGPASSWORD
-pg_dump -U $DATABASE_USER -h localhost $DATABASE_NAME > ../db_backup.sql
+pg_dump -U $DATABASE_USER -h localhost $DATABASE_NAME > ../src/lib/server/database/db_backup.sql
